43e2f9c92e6bcc02ee1c3847abcfd4d6e032f336
unknown <ezapataa1@eafit.edu.co>
2024-02-13 15:38:32 -0500

    Everything until activity 3

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,11 @@
-from django.urls import include, path 
-from .views import homePageView 
+from django.urls import path 
+from .views import ProductIndexView, ProductShowView, homePageView, AboutPageView, ContactPageView
 
 urlpatterns = [ 
-    path("", homePageView, name='home') ,
-    path("", include('pages.urls')),
+
+    path("", homePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('contact/', ContactPageView.as_view(), name='contact'),
+    path('products/', ProductIndexView.as_view(), name='index'), 
+    path('products/<str:id>', ProductShowView.as_view(), name='show'), 
 ] 
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,116 @@
 from django.shortcuts import render
-from django.http import HttpResponse # new 
+from django.views.generic import TemplateView 
+from django.views import View 
+ 
+ # Create your views here. 
 
-# Create your views here.
-def homePageView(request):
-    return HttpResponse('Hello World!') # new 
+class homePageView(TemplateView):
+    template_name = 'home.html'
+
+class AboutPageView(TemplateView): 
+    template_name = 'pages/about.html' 
+
+    def get_context_data(self, **kwargs): 
+
+        context = super().get_context_data(**kwargs) 
+
+        context.update({ 
+
+            "title": "About us - Online Store", 
+
+            "subtitle": "About us", 
+
+            "description": "This is an about page ...", 
+
+            "author": "Developed by: Your Name", 
+
+        }) 
+
+ 
+
+        return context 
+    
+class ContactPageView(TemplateView): 
+    template_name = 'pages/contact.html' 
+
+    def get_context_data(self, **kwargs): 
+
+        context = super().get_context_data(**kwargs) 
+
+        context.update({ 
+
+            "title": "Contact - Online Store", 
+
+            "subtitle": "Email: Test@test.com", 
+
+            "description": "Cel: 316 316 3162", 
+
+            "author": "Direction: Cra 59b # 209-34", 
+
+        }) 
+
+ 
+
+        return context 
+    
+class Product: 
+
+    products = [ 
+
+        {"id":"1", "name":"TV", "description":"Best TV", "price":200}, 
+
+        {"id":"2", "name":"iPhone", "description":"Best iPhone", "price":300}, 
+
+        {"id":"3", "name":"Chromecast", "description":"Best Chromecast", "price":400}, 
+
+        {"id":"4", "name":"Glasses", "description":"Best Glasses", "price":500} 
+
+    ] 
+
+ 
+
+class ProductIndexView(View): 
+
+    template_name = 'products/index.html' 
+
+ 
+
+    def get(self, request): 
+
+        viewData = {} 
+
+        viewData["title"] = "Products - Online Store" 
+
+        viewData["subtitle"] =  "List of products" 
+
+        viewData["products"] = Product.products 
+
+ 
+
+        return render(request, self.template_name, viewData) 
+
+ 
+
+class ProductShowView(View): 
+
+    template_name = 'products/show.html' 
+
+ 
+
+ 
+
+    def get(self, request, id): 
+
+        viewData = {} 
+
+        product = Product.products[int(id)-1] 
+
+        viewData["title"] = product["name"] + " - Online Store" 
+
+        viewData["subtitle"] =  product["name"] + " - Product information" 
+
+        viewData["product"] = product 
+
+ 
+
+        return render(request, self.template_name, viewData)
