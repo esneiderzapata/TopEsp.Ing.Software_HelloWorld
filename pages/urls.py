e89754388d45b50f1cb02df50cbb5402181a6e92
unknown <ezapataa1@eafit.edu.co>
2024-02-27 14:51:10 -0500

    Tutorial 08 b finished

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path 
-from .views import CartRemoveAllView, CartView, ProductIndexView, ProductShowView, homePageView, AboutPageView, ContactPageView, ProductCreateView, product_created
+from django.urls import path
+
+from pages.utils import ImageLocalStorage 
+from .views import CartRemoveAllView, CartView, ImageViewFactory, ImageViewNoDI, ProductIndexView, ProductShowView, homePageView, AboutPageView, ContactPageView, ProductCreateView, product_created
 
 urlpatterns = [ 
 
@@ -12,5 +14,9 @@ urlpatterns = [
     path('products/<str:id>', ProductShowView.as_view(), name='show'),
     path('cart/', CartView.as_view(), name='cart_index'), 
     path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'), 
-    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'), 
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'), 
+    path('image/save', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'),
+    path('imagenotdi/', ImageViewNoDI.as_view(), name='imagenodi_index'), 
+    path('image/save', ImageViewNoDI.as_view(), name='imagenodi_save'), 
 ] 
--- pages/views.py
+++ pages/views.py
@@ -4,7 +4,9 @@ from django.views.generic import TemplateView, ListView
 from django.views import View 
 from django.http import HttpResponseRedirect
 from django import forms 
-from django.shortcuts import render, redirect 
+from django.shortcuts import render, redirect
+
+from pages.utils import ImageLocalStorage 
 from .models import Product 
 
 def positiveValidator(value):
@@ -149,3 +151,31 @@ class CartRemoveAllView(View):
             del request.session['cart_product_data'] 
 
         return redirect('cart_index') 
+    
+def ImageViewFactory(image_storage): 
+    class ImageView(View): 
+        template_name = 'images/index.html' 
+
+        def get(self, request): 
+            image_url = request.session.get('image_url', '') 
+            return render(request, self.template_name, {'image_url': image_url}) 
+
+        def post(self, request): 
+            image_url = image_storage.store(request) 
+            request.session['image_url'] = image_url 
+            return redirect('image_index') 
+
+    return ImageView 
+
+class ImageViewNoDI(View): 
+    template_name = 'images/index.html' 
+
+    def get(self, request): 
+        image_url = request.session.get('image_url', '') 
+        return render(request, self.template_name, {'image_url': image_url}) 
+
+    def post(self, request): 
+        image_storage = ImageLocalStorage() 
+        image_url = image_storage.store(request) 
+        request.session['image_url'] = image_url 
+        return redirect('image_index')
